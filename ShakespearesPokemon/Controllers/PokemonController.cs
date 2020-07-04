4b38df7a8c4d96d4f22462ed3e6b75b252cb0c33
Aishwarya06 <apabbisetti@gmail.com>
2020-07-04 20:43:55 +0200

    create .NET Core Webapi project

---
+++ ShakespearesPokemon/Controllers/PokemonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShakespearesPokemon.Models;
+
+namespace ShakespearesPokemon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+       
+
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        //GET: api/Pokemon
+        [HttpGet]
+        public ActionResult<Pokemon> Get()
+        {
+            return new Pokemon
+            {
+                Name = "Charizard",
+                Description = "I am Charizard"
+            };
+        }
+    }
+}
